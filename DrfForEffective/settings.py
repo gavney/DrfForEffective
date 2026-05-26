94c2ea0e6cc473f4c6078272ddbb8f9b4b25f7e6
lebed <lebed@DESKTOP-MES3OJ6>
2026-05-26 11:10:42 +0300

    UserModel

--- DrfForEffective/settings.py
+++ DrfForEffective/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+
+    'users',
+    'authentication',
+    'permissions',
+    'business',
 ]
 
 MIDDLEWARE = [
@@ -68,14 +75,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DrfForEffective.wsgi.application'
 
+AUTH_USER_MODEL = 'users.User'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drfForJob',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
